1b81b0c2ad6f4306d9c16de5764a5e1981c7952c
Mio <mio@mio19.uk>
2025-10-31 16:09:52 +1100

    Revert "fix"
    This reverts commit ed49b241ad64f8e78086e1c5fe0ee8d2bee19ecf.

--- default.nix
+++ default.nix
@@ -117,14 +117,8 @@ in
   minetest591 = pkgs.callPackage ./pkgs/minetest591 {
     stdenv = v3Optimizations pkgs.clangStdenv;
   };
-  minetest591client = pkgs.callPackage ./pkgs/minetest591 {
-    stdenv = v3Optimizations pkgs.clangStdenv;
-    buildServer = false;
-  };
-  minetest591server = pkgs.callPackage ./pkgs/minetest591 {
-    stdenv = v3Optimizations pkgs.clangStdenv;
-    buildClient = false;
-  };
+  minetest591client = minetest591.override { buildServer = false; };
+  minetest591server = minetest591.override { buildClient = false; };
   irrlichtmt = pkgs.callPackage ./pkgs/irrlichtmt {
     stdenv = v3Optimizations pkgs.clangStdenv;
   };
@@ -132,22 +126,12 @@ in
     irrlichtmt = irrlichtmt;
     stdenv = v3Optimizations pkgs.clangStdenv;
   };
-  minetest580client = pkgs.callPackage ./pkgs/minetest580 {
-    irrlichtmt = irrlichtmt;
-    stdenv = v3Optimizations pkgs.clangStdenv;
-    buildServer = false;
-  };
-  minetest580-touch = pkgs.callPackage ./pkgs/minetest580 {
-    irrlichtmt = irrlichtmt;
-    stdenv = v3Optimizations pkgs.clangStdenv;
+  minetest580client = minetest580.override { buildServer = false; };
+  minetest580-touch = minetest580.override {
     buildServer = false;
     withTouchSupport = true;
   };
-  minetest580server = pkgs.callPackage ./pkgs/minetest580 {
-    irrlichtmt = irrlichtmt;
-    stdenv = v3Optimizations pkgs.clangStdenv;
-    buildClient = false;
-  };
+  minetest580server = minetest580.override { buildClient = false; };
   musescore3 =
     if pkgs.stdenv.isDarwin then
       pkgs.callPackage ./pkgs/musescore3/darwin.nix { }

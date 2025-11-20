c8567501157a0509f37b5950018d729f081b1c93
Mio <mio@mio19.uk>
2025-11-20 12:42:55 +1100

    remove gitlens&fish

--- default.nix
+++ default.nix
@@ -308,11 +308,6 @@ rec {
   };
   # https://github.com/NixOS/nixpkgs/pull/461412
   shell-gpt = pkgs.callPackage ./pkgs/shell-gpt/package.nix { };
-  # https://github.com/NixOS/nixpkgs/issues/462082
-  vscode-extensions.eamodio.gitlens = pkgs.callPackage ./pkgs/eamodio.gitlens {
-  };
-  # https://github.com/NixOS/nixpkgs/pull/461779
-  fish = pkgs.callPackage ./pkgs/fish/package.nix { };
   /*
     mygui = v3overrideAttrs (
       pkgs.mygui.overrideAttrs (old: {

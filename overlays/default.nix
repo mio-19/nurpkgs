8f06e407478361644dfa5243ed54310b8a12134e
Mio <mio@mio19.uk>
2025-12-02 14:27:43 +1100

    howdy = final.nur.repos.mio.howdy;

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,5 +4,6 @@
   # my-overlay = import ./my-overlay;
   default = final: prev: {
     linux-enable-ir-emitter = final.nur.repos.mio.linux-enable-ir-emitter;
+    howdy = final.nur.repos.mio.howdy;
   };
 }

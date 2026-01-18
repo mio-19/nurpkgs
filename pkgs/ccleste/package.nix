e69112166b46e537ee01271e3e43dc2c74c71426
Mio <mio@mio19.uk>
2026-01-18 20:53:36 +1100

    patch

--- pkgs/ccleste/package.nix
+++ pkgs/ccleste/package.nix
@@ -35,34 +35,41 @@ stdenv.mkDerivation rec {
       exec = "ccleste";
       icon = "ccleste";
       comment = "Celeste Classic C source port";
-      categories = [ "Game" "ActionGame" ];
+      categories = [
+        "Game"
+        "ActionGame"
+      ];
     })
+  ];
+
+  patches = [
+    ./fix-pitch.patch
   ];
 
   postPatch = ''
     substituteInPlace sdl12main.c \
       --replace-fail 'snprintf(path, n, "data%c%s", pathsep, fname);' \
-                'snprintf(path, n, "'$out'/share/ccleste/data%c%s", pathsep, fname);' \
-      --replace-fail 'data[x*scale+i + (y*scale+j)*w*scale] = pix;' \
-                'data[x*scale+i + (y*scale+j)*surf->pitch] = pix;'
+                'snprintf(path, n, "'$out'/share/ccleste/data%c%s", pathsep, fname);'
   '';
 
-  installPhase = let
-    icon = fetchurl {
-      url = "https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/Papirus/64x64/apps/celeste.svg";
-      sha256 = "sha256-PS43zAYy/+oNm8ykTvi/aGv8vWxVtqnbaKtVJI/prNY=";
-    };
-  in ''
-    runHook preInstall
-    mkdir -p $out/bin $out/share/ccleste $out/share/icons/hicolor/scalable/apps
-    cp ccleste $out/bin/
-    cp -r data $out/share/ccleste/
-    
-    # Install icon
-    ln -s ${icon} $out/share/icons/hicolor/scalable/apps/ccleste.svg
-    
-    runHook postInstall
-  '';
+  installPhase =
+    let
+      icon = fetchurl {
+        url = "https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/Papirus/64x64/apps/celeste.svg";
+        sha256 = "sha256-PS43zAYy/+oNm8ykTvi/aGv8vWxVtqnbaKtVJI/prNY=";
+      };
+    in
+    ''
+      runHook preInstall
+      mkdir -p $out/bin $out/share/ccleste $out/share/icons/hicolor/scalable/apps
+      cp ccleste $out/bin/
+      cp -r data $out/share/ccleste/
+
+      # Install icon
+      ln -s ${icon} $out/share/icons/hicolor/scalable/apps/ccleste.svg
+
+      runHook postInstall
+    '';
 
   meta = with lib; {
     description = "Celeste Classic C source port";

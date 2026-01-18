45165b1d13cb674cdcef1c056d861a8754c090d5
Mio <mio@mio19.uk>
2026-01-19 00:58:57 +1100

    fixup

--- pkgs/rain/package.nix
+++ pkgs/rain/package.nix
@@ -2,6 +2,11 @@
   lib,
   flutter338,
   fetchFromGitHub,
+  runCommand,
+  yq-go,
+  _experimental-update-script-combinators,
+  gitUpdater,
+  dart,
 }:
 
 flutter338.buildFlutterApplication rec {
@@ -16,6 +21,51 @@ flutter338.buildFlutterApplication rec {
   };
 
   pubspecLock = lib.importJSON ./pubspec.lock.json;
+  gitHashes = lib.importJSON ./git-hashes.json;
+
+  # Ensure Isar can dlopen libisar.so when the binary is invoked via a symlink.
+  extraWrapProgramArgs = ''
+    --prefix LD_LIBRARY_PATH : $out/app/${pname}/lib
+  '';
+
+  passthru = {
+    pubspecSource =
+      runCommand "pubspec.lock.json"
+        {
+          inherit src;
+          nativeBuildInputs = [ yq-go ];
+        }
+        ''
+          yq eval --output-format=json --prettyPrint $src/pubspec.lock > "$out"
+        '';
+    updateScript = _experimental-update-script-combinators.sequence [
+      (
+        (gitUpdater {
+          ignoredVersions = ".*(rc|beta).*";
+          rev-prefix = "v";
+        })
+        // {
+          supportedFeatures = [ ];
+        }
+      )
+      (
+        (_experimental-update-script-combinators.copyAttrOutputToFile "rain.pubspecSource" ./pubspec.lock.json)
+        // {
+          supportedFeatures = [ ];
+        }
+      )
+      {
+        command = [
+          dart.fetchGitHashesScript
+          "--input"
+          ./pubspec.lock.json
+          "--output"
+          ./git-hashes.json
+        ];
+        supportedFeatures = [ ];
+      }
+    ];
+  };
 
   meta = {
     description = "Weather application";

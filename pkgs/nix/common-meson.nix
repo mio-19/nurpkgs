9f2c14cf7b01408d1a471a9e9488f99b04577158
Mio <mio@mio19.uk>
2026-02-14 10:32:03 +1100

    Revert "stable nix"
    This reverts commit a7674d0edd8a85dd0ad15e83f929abcef2146a28.

--- pkgs/nix/common-meson.nix
+++ pkgs/nix/common-meson.nix
@@ -11,11 +11,6 @@
     inherit hash;
   },
   patches ? [ ],
-  maintainers ? [
-    lib.maintainers.artturin
-    lib.maintainers.philiptaron
-    lib.maintainers.lovesegfault
-  ],
   teams ? [ lib.teams.nix ],
   self_attribute_name,
 }@args:
@@ -261,7 +256,7 @@ stdenv.mkDerivation (finalAttrs: {
   # point 'nix edit' and ofborg at the file that defines the attribute,
   # not this common file.
   pos = builtins.unsafeGetAttrPos "version" args;
-  meta = with lib; {
+  meta = {
     description = "Powerful package manager that makes package management reliable and reproducible";
     longDescription = ''
       Nix is a powerful package manager for Linux and other Unix systems that
@@ -272,13 +267,13 @@ stdenv.mkDerivation (finalAttrs: {
     '';
     homepage = "https://nixos.org/";
     license = lib.licenses.lgpl21Plus;
-    inherit maintainers teams;
+    inherit teams;
     platforms = lib.platforms.unix;
     # Gets stuck in functional-tests in cross-trunk jobset and doesn't timeout
     # https://hydra.nixos.org/build/298175022
     # probably https://github.com/NixOS/nix/issues/13042
     broken = stdenv.hostPlatform.system == "i686-linux" && stdenv.buildPlatform != stdenv.hostPlatform;
-    outputsToInstall = [ "out" ] ++ optional enableDocumentation "man";
+    outputsToInstall = [ "out" ] ++ lib.optional enableDocumentation "man";
     mainProgram = "nix";
   };
 })

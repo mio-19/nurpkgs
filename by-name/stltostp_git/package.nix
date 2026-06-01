62c7e5fbfe7fe8b769db6461f8fe018743c28bcb
Mio <mio@mio19.uk>
2026-06-01 22:16:41 +1000

    feat: add stltostp 1.0.1 and rename master to stltostp_git

--- by-name/stltostp_git/package.nix
+++ by-name/stltostp_git/package.nix
@@ -6,26 +6,17 @@
 }:
 
 stdenv.mkDerivation rec {
-  pname = "stltostp";
-  version = "1.0.1";
+  pname = "stltostp_git";
+  version = "unstable-2025-11-27";
 
   src = fetchFromGitHub {
     owner = "slugdev";
     repo = "stltostp";
-    rev = "v${version}";
-    sha256 = "1bqr6w5n082nj16cr6wh90n9xxiwaa2v36mmy4v8fqrif1wqf3p2";
+    rev = "b22cfbda0cfb79add475ec66939e6eb714e00082";
+    sha256 = "1hpx15jc0hp4likvqnnh93wmh1d1q0vm60izi1m63fgxm7grkcb2";
   };
 
   nativeBuildInputs = [ cmake ];
-
-  postPatch = ''
-    sed -i 's/float_t/float/g' main.cpp
-    sed -i 's/float_t/float/g' StepKernel.h
-    sed -i 's/float_t/float/g' StepKernel.cpp
-    sed -i '1i#include <cmath>' StepKernel.cpp
-    sed -i '1i#include <cstdint>' main.cpp
-    sed -i 's/std::string StepKernel::read_line/std::string read_line/g' StepKernel.h
-  '';
 
   meta = with lib; {
     description = "Command line utility to convert stl files to STEP brep files";

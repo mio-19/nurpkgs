fec48649e44080724cfdadf9600ede249e3e7c5c
Mio <mio@mio19.uk>
2026-06-30 20:36:19 +1000

    refactor: rename grub2 to grub2-patched and move to by-name

---
+++ by-name/grub2-patched/package.nix
@@ -0,0 +1,16 @@
+{
+  pkgs,
+  grub2,
+}:
+
+let
+  private = import ../../private.nix { inherit pkgs; };
+in
+private.nodarwin (
+  private.v3overridegcc (
+    grub2.overrideAttrs (old: {
+      pname = "grub2-patched";
+      patches = (old.patches or [ ]) ++ [ ../../patches/grub-os-prober-title.patch ];
+    })
+  )
+)

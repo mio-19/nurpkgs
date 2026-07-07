f8e28264444c8782a8c3dcf4717b1aa95db1d6ff
Mio <mio@mio19.uk>
2026-07-08 09:22:38 +1000

    fixup! autodesk-fusion-360: init at 8.0.0

--- by-name/autodesk-fusion-360/package.nix
+++ by-name/autodesk-fusion-360/package.nix
@@ -29,11 +29,12 @@ stdenv.mkDerivation rec {
     hash = "sha256-f9BvoDKZePn0uRdUEppfzyuO+xW2EW1PLvHN9PAo3QI=";
   };
 
-
-
   patches = [ ./nix-env.patch ];
 
-  nativeBuildInputs = [ makeWrapper copyDesktopItems ];
+  nativeBuildInputs = [
+    makeWrapper
+    copyDesktopItems
+  ];
 
   desktopItems = [
     (makeDesktopItem {
@@ -42,41 +43,63 @@ stdenv.mkDerivation rec {
       icon = "autodesk-fusion-360";
       desktopName = "Autodesk Fusion 360";
       genericName = "CAD Application";
-      categories = [ "Education" "Engineering" ];
+      categories = [
+        "Education"
+        "Engineering"
+      ];
       startupNotify = true;
+      terminal = true;
     })
   ];
 
   installPhase = ''
-    runHook preInstall
+        runHook preInstall
 
-    mkdir -p $out/bin $out/libexec $out/share/autodesk-fusion-360 $out/share/icons/hicolor/scalable/apps
-    cp -r files/* $out/share/autodesk-fusion-360/
-    
-    # Install icon
-    cp files/images/launcher/Autodesk\ Fusion\ 360-launcher-xterm.png $out/share/icons/hicolor/scalable/apps/autodesk-fusion-360.png
+        mkdir -p $out/bin $out/libexec $out/share/autodesk-fusion-360 $out/share/icons/hicolor/scalable/apps
+        cp -r files/* $out/share/autodesk-fusion-360/
+        
+        # Install icon
+        cp files/images/launcher/Autodesk\ Fusion\ 360-launcher-xterm.png $out/share/icons/hicolor/scalable/apps/autodesk-fusion-360.png
 
-    chmod +x $out/share/autodesk-fusion-360/setup/autodesk_fusion_installer_x86-64.sh
-    makeWrapper $out/share/autodesk-fusion-360/setup/autodesk_fusion_installer_x86-64.sh $out/libexec/autodesk-fusion-installer \
-      --prefix PATH : ${lib.makeBinPath [ bash wine winetricks cabextract p7zip curl samba zenity yad ]}
+        chmod +x $out/share/autodesk-fusion-360/setup/autodesk_fusion_installer_x86-64.sh
+        makeWrapper $out/share/autodesk-fusion-360/setup/autodesk_fusion_installer_x86-64.sh $out/libexec/autodesk-fusion-installer \
+          --prefix PATH : ${
+            lib.makeBinPath [
+              bash
+              wine
+              winetricks
+              cabextract
+              p7zip
+              curl
+              samba
+              zenity
+              yad
+            ]
+          }
 
-    chmod +x $out/share/autodesk-fusion-360/setup/data/autodesk_fusion_launcher.sh
-    makeWrapper $out/share/autodesk-fusion-360/setup/data/autodesk_fusion_launcher.sh $out/libexec/autodesk-fusion-launcher \
-      --prefix PATH : ${lib.makeBinPath [ bash wine curl ]}
+        chmod +x $out/share/autodesk-fusion-360/setup/data/autodesk_fusion_launcher.sh
+        makeWrapper $out/share/autodesk-fusion-360/setup/data/autodesk_fusion_launcher.sh $out/libexec/autodesk-fusion-launcher \
+          --prefix PATH : ${
+            lib.makeBinPath [
+              bash
+              wine
+              curl
+            ]
+          }
 
-    cat > $out/bin/autodesk-fusion-360 << EOF
-#!${runtimeShell}
-if [ ! -d "\$HOME/.autodesk_fusion/wineprefixes/default" ]; then
-    echo "First run detected, starting installer..."
-    exec $out/libexec/autodesk-fusion-installer --install
-else
-    echo "Starting Autodesk Fusion 360..."
-    exec $out/libexec/autodesk-fusion-launcher
-fi
-EOF
-    chmod +x $out/bin/autodesk-fusion-360
+        cat > $out/bin/autodesk-fusion-360 << EOF
+    #!${runtimeShell}
+    if [ ! -d "\$HOME/.autodesk_fusion/wineprefixes/default" ]; then
+        echo "First run detected, starting installer..."
+        exec $out/libexec/autodesk-fusion-installer --install
+    else
+        echo "Starting Autodesk Fusion 360..."
+        exec $out/libexec/autodesk-fusion-launcher
+    fi
+    EOF
+        chmod +x $out/bin/autodesk-fusion-360
 
-    runHook postInstall
+        runHook postInstall
   '';
 
   meta = with lib; {

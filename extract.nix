fc0ef90af9ce7df6cc9fedc4aedf4211b286fbc0
Mio <mio@mio19.uk>
2026-07-09 09:10:43 +1000

    chore: remove extract.nix utility script

--- extract.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> { };
-appimageTools.extract {
-  pname = "beam-studio";
-  version = "2.6.8";
-  src = fetchurl {
-    url = "https://github.com/flux3dp/beam-studio/releases/download/v2.6.8/Beam.Studio-2.6.8.AppImage";
-    hash = "sha256-R4F1eZ947Yy5DndqFzQW/cW3V7p/G65L1K4e7WfH4g4=";
-  };
-}

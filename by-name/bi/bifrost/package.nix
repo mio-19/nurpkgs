864fe89403376b731fbdec884ef433d0a9e1f47b
Mio <mio@mio19.uk>
2026-08-02 17:17:25 +1000

    Revert "try this bifrost"
    This reverts commit 7601edb6a120b0d1bdc627edee0e2faa730efe4c.

--- by-name/bi/bifrost/package.nix
+++ by-name/bi/bifrost/package.nix
@@ -152,8 +152,8 @@ stdenv.mkDerivation (finalAttrs: {
 
         makeWrapper $out/opt/bifrost/bin/Bifrost $out/bin/Bifrost \
           --run '
-            if [ -z "''${GDK_SCALE:-}" ] && [ "''${XDG_CURRENT_DESKTOP:-}" = "KDE" ]; then
-              SCALE=$(${lib.getExe' kdePackages.kconfig "kreadconfig6"} --group KScreen --key ScaleFactor 2>/dev/null || true)
+            if [ -z "''${GDK_SCALE:-}" ] && [ "''${XDG_CURRENT_DESKTOP:-}" = "KDE" ] && command -v kreadconfig6 >/dev/null; then
+              SCALE=$(kreadconfig6 --group KScreen --key ScaleFactor 2>/dev/null || true)
               if [ -n "$SCALE" ]; then
                 export _JAVA_OPTIONS="-Dsun.java2d.uiScale=$SCALE ''${_JAVA_OPTIONS:-}"
               fi

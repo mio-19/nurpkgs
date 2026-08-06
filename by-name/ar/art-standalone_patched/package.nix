f863bcd47ec1fcda0f1d897795184d61c6736ca1
Mio <mio@mio19.uk>
2026-08-06 15:40:10 +1000

    chore: copy wolfssl from correct nixpkgs commit and enable jni

--- by-name/ar/art-standalone_patched/package.nix
+++ by-name/ar/art-standalone_patched/package.nix
@@ -3,5 +3,5 @@
 art-standalone.overrideAttrs (old: {
   pname = "art-standalone-patched";
   patches = builtins.filter (p: baseNameOf p != "remove-wolfssljni.patch") old.patches;
-  buildInputs = old.buildInputs ++ [ wolfssl ];
+  buildInputs = old.buildInputs ++ [ (wolfssl.override { enableJni = true; }) ];
 })

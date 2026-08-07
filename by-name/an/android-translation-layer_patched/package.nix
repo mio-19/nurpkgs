681481b70523d315cbf13a43167aacba0434740a
Mio <mio@mio19.uk>
2026-08-07 19:55:03 +1000

    feat(atl): stub GMS Tasks API

--- by-name/an/android-translation-layer_patched/package.nix
+++ by-name/an/android-translation-layer_patched/package.nix
@@ -38,6 +38,7 @@
       ./android-translation-layer-gms-availability-stub.patch
       ./android-translation-layer-firebase-stubs.patch
       ./android-translation-layer-gms-client-stubs.patch
+      ./android-translation-layer-gms-tasks-stubs.patch
     ];
     postInstall = (old.postInstall or "") + ''
       mkdir -p $out/etc/security

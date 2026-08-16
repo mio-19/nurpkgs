3e1e8b7a4711558cb77492d84b634868f95b6907
Mio <mio@mio19.uk>
2026-08-17 01:18:28 +1000

    fix: build uplink on linux

--- by-name/up/uplink/package.nix
+++ by-name/up/uplink/package.nix
@@ -25,110 +25,12 @@ let
   buildFlutterApp = callPackage ./build-support/build-flutter-application.nix { };
 in
 lib.overrideDerivation
-  (buildFlutterApp {
+  (buildFlutterApp ({
     pname = "uplink";
     version = "0.1.0";
     src = ./.;
 
     targetFlutterPlatform = "linux"; # Bypass nixpkgs assertion, we will override buildPhase
-
-    buildPhase =
-      if stdenv.hostPlatform.isDarwin then
-        ''
-          runHook preBuild
-          export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
-          export PATH="$DEVELOPER_DIR/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
-
-          # CoreFoundation bypasses HOME, so we need to set CFFIXED_USER_HOME
-          export CFFIXED_USER_HOME=$HOME
-
-          # Create a fake Xcode bundle to wrap xcodebuild without breaking xcrun validation
-          export REAL_DEV_DIR=/Applications/Xcode.app/Contents/Developer
-          export FAKE_XCODE="$(pwd)/FakeXcode.app"
-          export FAKE_DEV_DIR="$FAKE_XCODE/Contents/Developer"
-
-          mkdir -p "$FAKE_DEV_DIR/usr/bin"
-
-          # Symlink the required plists for xcrun to consider this a valid Xcode
-          ln -s /Applications/Xcode.app/Contents/Info.plist "$FAKE_XCODE/Contents/"
-          ln -s /Applications/Xcode.app/Contents/version.plist "$FAKE_XCODE/Contents/"
-
-          # Symlink all contents of the real developer dir
-          for file in "$REAL_DEV_DIR"/*; do
-              if [[ "$(basename "$file")" != "usr" ]]; then
-                  ln -s "$file" "$FAKE_DEV_DIR/"
-              fi
-          done
-
-          # Handle usr/
-          mkdir -p "$FAKE_DEV_DIR/usr"
-          for file in "$REAL_DEV_DIR/usr"/*; do
-              if [ "$(basename "$file")" != "bin" ]; then
-                  ln -s "$file" "$FAKE_DEV_DIR/usr/"
-              fi
-          done
-          mkdir -p "$FAKE_DEV_DIR/usr/bin"
-          for file in "$REAL_DEV_DIR/usr/bin"/*; do
-              ln -s "$file" "$FAKE_DEV_DIR/usr/bin/"
-          done
-
-          # Replace xcodebuild with our wrapper
-          rm -f "$FAKE_DEV_DIR/usr/bin/xcodebuild"
-          cat << 'EOF2' > "$FAKE_DEV_DIR/usr/bin/xcodebuild"
-          #!/bin/bash
-          exec "$REAL_DEV_DIR/usr/bin/xcodebuild" ARCHS=$(uname -m) ONLY_ACTIVE_ARCH=YES -IDEPackageSupportDisableManifestSandbox=YES -IDEPackageSupportDisablePluginExecutionSandbox=YES "$@"
-          EOF2
-          chmod +x "$FAKE_DEV_DIR/usr/bin/xcodebuild"
-
-          mkdir -p "$(pwd)/custom_bin"
-          cat << 'EOF3' > "$(pwd)/custom_bin/rsync"
-          #!/bin/bash
-          /usr/bin/rsync "$@"
-          status=$?
-          if [[ "$status" == 0 ]]; then
-              for arg in "$@"; do
-                  last_arg="$arg"
-              done
-              if [[ -d "$last_arg" ]]; then
-                  chmod -R u+w "$last_arg" || true
-              fi
-          fi
-          exit $status
-          EOF3
-          chmod +x "$(pwd)/custom_bin/rsync"
-
-          export DEVELOPER_DIR="$FAKE_DEV_DIR"
-          export PATH="$(pwd)/custom_bin:$PATH"
-
-          # Nixpkgs sets LD=ld, which causes xcodebuild to invoke ld directly
-          # but still pass -Xlinker flags meant for clang. Unset it to use default.
-          unset LD
-
-          flutter build macos -v --release
-          runHook postBuild
-        ''
-      else
-        null;
-
-    installPhase =
-      if stdenv.hostPlatform.isDarwin then
-        ''
-          runHook preInstall
-          mkdir -p $out/Applications
-          cp -r build/macos/Build/Products/Release/uplink.app $out/Applications/
-          runHook postInstall
-        ''
-      else
-        null;
-
-    sandboxProfile =
-      if stdenv.hostPlatform.isDarwin then
-        ''
-          (allow file-read* file-write* process-exec mach-lookup)
-          (deny file-read* file-write* process-exec mach-lookup (subpath "/usr/local") (with no-log))
-        ''
-      else
-        "";
 
     autoPubspecLock = ./pubspec.lock;
 
@@ -141,13 +43,13 @@ lib.overrideDerivation
       rustc
       pkg-config
       cmake
-      cocoapods
       cacert
       ninja
       rustPlatform.cargoSetupHook
       clang
     ]
     ++ lib.optionals stdenv.hostPlatform.isDarwin [
+      cocoapods
       writableTmpDirAsHomeHook
     ]
     ++ lib.optionals stdenv.hostPlatform.isLinux [
@@ -237,7 +139,93 @@ lib.overrideDerivation
       maintainers = [ ];
       platforms = platforms.unix;
     };
-  })
-  (old: {
+  } // lib.optionalAttrs stdenv.hostPlatform.isDarwin {
+    buildPhase = ''
+      runHook preBuild
+      export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
+      export PATH="$DEVELOPER_DIR/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+
+      # CoreFoundation bypasses HOME, so we need to set CFFIXED_USER_HOME
+      export CFFIXED_USER_HOME=$HOME
+
+      # Create a fake Xcode bundle to wrap xcodebuild without breaking xcrun validation
+      export REAL_DEV_DIR=/Applications/Xcode.app/Contents/Developer
+      export FAKE_XCODE="$(pwd)/FakeXcode.app"
+      export FAKE_DEV_DIR="$FAKE_XCODE/Contents/Developer"
+
+      mkdir -p "$FAKE_DEV_DIR/usr/bin"
+
+      # Symlink the required plists for xcrun to consider this a valid Xcode
+      ln -s /Applications/Xcode.app/Contents/Info.plist "$FAKE_XCODE/Contents/"
+      ln -s /Applications/Xcode.app/Contents/version.plist "$FAKE_XCODE/Contents/"
+
+      # Symlink all contents of the real developer dir
+      for file in "$REAL_DEV_DIR"/*; do
+          if [[ "$(basename "$file")" != "usr" ]]; then
+              ln -s "$file" "$FAKE_DEV_DIR/"
+          fi
+      done
+
+      # Handle usr/
+      mkdir -p "$FAKE_DEV_DIR/usr"
+      for file in "$REAL_DEV_DIR/usr"/*; do
+          if [ "$(basename "$file")" != "bin" ]; then
+              ln -s "$file" "$FAKE_DEV_DIR/usr/"
+          fi
+      done
+      mkdir -p "$FAKE_DEV_DIR/usr/bin"
+      for file in "$REAL_DEV_DIR/usr/bin"/*; do
+          ln -s "$file" "$FAKE_DEV_DIR/usr/bin/"
+      done
+
+      # Replace xcodebuild with our wrapper
+      rm -f "$FAKE_DEV_DIR/usr/bin/xcodebuild"
+      cat << 'EOF2' > "$FAKE_DEV_DIR/usr/bin/xcodebuild"
+      #!/bin/bash
+      exec "$REAL_DEV_DIR/usr/bin/xcodebuild" ARCHS=$(uname -m) ONLY_ACTIVE_ARCH=YES -IDEPackageSupportDisableManifestSandbox=YES -IDEPackageSupportDisablePluginExecutionSandbox=YES "$@"
+      EOF2
+      chmod +x "$FAKE_DEV_DIR/usr/bin/xcodebuild"
+
+      mkdir -p "$(pwd)/custom_bin"
+      cat << 'EOF3' > "$(pwd)/custom_bin/rsync"
+      #!/bin/bash
+      /usr/bin/rsync "$@"
+      status=$?
+      if [[ "$status" == 0 ]]; then
+          for arg in "$@"; do
+              last_arg="$arg"
+          done
+          if [[ -d "$last_arg" ]]; then
+              chmod -R u+w "$last_arg" || true
+          fi
+      fi
+      exit $status
+      EOF3
+      chmod +x "$(pwd)/custom_bin/rsync"
+
+      export DEVELOPER_DIR="$FAKE_DEV_DIR"
+      export PATH="$(pwd)/custom_bin:$PATH"
+
+      # Nixpkgs sets LD=ld, which causes xcodebuild to invoke ld directly
+      # but still pass -Xlinker flags meant for clang. Unset it to use default.
+      unset LD
+
+      flutter build macos -v --release
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/Applications
+      cp -r build/macos/Build/Products/Release/uplink.app $out/Applications/
+      runHook postInstall
+    '';
+
+    sandboxProfile = ''
+      (allow file-read* file-write* process-exec mach-lookup)
+      (deny file-read* file-write* process-exec mach-lookup (subpath "/usr/local") (with no-log))
+    '';
+  }))
+  (old: lib.optionalAttrs stdenv.hostPlatform.isDarwin {
     __noChroot = true;
   })

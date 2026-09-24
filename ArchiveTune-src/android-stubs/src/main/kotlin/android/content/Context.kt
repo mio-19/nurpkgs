0456de326157c6a887e418f6a7687e313a51e09f
Mio <mio@mio19.uk>
2026-09-24 13:32:22 +1000

    chore: clean up android resources and apply desktop build logic

--- ArchiveTune-src/android-stubs/src/main/kotlin/android/content/Context.kt
+++
@@ -1,5 +0,0 @@
-package android.content
-open class Context {
-    fun getString(id: Int): String = "Stub"
-    fun getSystemService(name: String): Any? = null
-}
--- ArchiveTune-src/app/src/main/kotlin/moe/rukamori/archivetune/ui/component/OptionStats.kt
+++
@@ -1,4 +0,0 @@
-package moe.rukamori.archivetune.ui.component
-enum class OptionStats {
-    MOST_PLAYED, RECENTLY_PLAYED, RECENTLY_ADDED, RECENTLY_DOWNLOADED
-}
